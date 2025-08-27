319bcd953588dbf459236eda406454eed5d8de87
Vijaydev19 <dev19.ultroneous@gmail.com>
2025-08-27 13:25:58 +0530

    feat: created hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Hero from "@/component/home/Hero";
+
 export default function Home() {
   return (
     <>
@@ -20,9 +22,9 @@ export default function Home() {
           }),
         }}
       />
-      <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-        welcome to gologistic logistic management system
-      </div>
+      <>
+        <Hero />
+      </>
     </>
   );
 }
