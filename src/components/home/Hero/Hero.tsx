32729154b0a6906445a4b2341df20dd51911b6ab
Vijaydev19 <dev19.ultroneous@gmail.com>
2025-12-31 15:45:29 +0530

    fix: fixed height on shipping services slider

--- src/components/home/Hero/Hero.tsx
+++ src/components/home/Hero/Hero.tsx
@@ -22,13 +22,13 @@ export const Hero: FC<HeroSectionProps> = ({
     <Container backgroundClassName="pt-20" className="relative" id="home" ref={heroRef}>
       <Image
         alt={backgroundImage.alt}
-        className="absolute -z-20 h-full w-full object-cover"
-        height={470}
+        className="absolute -z-20 w-full object-cover"
+        fetchPriority="high"
+        fill
         priority
-        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 100vw, 1440px"
+        sizes="100vw"
         src={backgroundImage.src}
         title={backgroundImage.alt}
-        width={1440}
       />
       <div className="flex flex-col gap-2 px-4 pb-20 md:flex-row md:items-center md:gap-4 md:px-6 md:pb-19 xl:items-start xl:gap-11.5 xl:px-17.5 xl:pt-37.5 xl:pb-31.5">
         <h1
